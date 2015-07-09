73291a6eb8063ed0f27df02ddb2bd192baeea0ec
Michael <michael@cyanshift.com>
2015-07-09 22:35:31 +0200

    First commit

---
+++ Pitch Perfect/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Michael Palka on Jul/9/2015.
+//  Copyright (c) 2015 Cyanshift Apps AB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO: Show text "recording in progress"
+        //TODO: Record the user's coice
+        println("pressed button")
+    }
+}
+
